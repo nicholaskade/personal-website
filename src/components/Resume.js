5c73a4a88e1584cc3c15f7253ee9b901efd0ef75
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2024-10-01 21:25:46 -0400

    Updated section order

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,9 +8,9 @@ function Resume() {
     return (
         <>
             <Skills/>
+            <Experience />
             <Projects />
             <Education />
-            <Experience />
         </>
     )
 };
